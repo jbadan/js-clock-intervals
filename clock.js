6ea7e99e844723f7c22f21bad310336184b74613
jbadan <jenna.badanowski@gmail.com>
2017-08-31 19:10:01 -0700

    clock in real time

--- clock.js
+++ clock.js
@@ -1,41 +1,91 @@
 
-// function initLocalClocks() {
-//   var date = new Date;
-//   var seconds = date.getSeconds();
-//   var minutes = date.getMinutes();
-//   var hours = date.getHours();
+
+// function rotateSecondHand(){
+//   var secondHand = document.getElementById("second");  
+//   var counter = 0;
+//   var loop = setInterval(function() {
+//       counter++;
+//       secondHand.style.transform = "rotate(" + counter + "deg)";
+//   }, 1000);
+// }
+
+// rotateSecondHand();
+
+// function rotateMinuteHand(){
+//   var minuteHand = document.getElementById("minute");
+//   var counter = 0;
+//   var loop = setInterval(function(){
+//     counter+=6; 
+//     minuteHand.style.transform = "rotate(" + counter + "deg)";
+//   }, 60000); 
+// }
+
+// rotateMinuteHand();
+
+// function rotateHourHand(){
+//   var hourHand = document.getElementById("hour");
+//   var counter = 0;
+//   var loop = setInterval(function(){
+//     counter+=30;
+//     hourHand.style.transform = "rotate(" + counter + "deg)";
+//   }, 3.6e+6); 
+// }
+
+// rotateHourHand();
 
 
+//minute * 6 = degrees 
+//second  = degrees 
+//hour * 30 = degrees
 
-function rotateSecondHand(){
-  var secondHand = document.getElementById("second");  
-  var counter = 0;
-  setInterval(function() {
-    counter++;
-    secondHand.style.transform = "rotate(" + counter + "deg)";
-  }, 1000);
+var now = new Date();
+var seconds = now.getSeconds();
+var minutes = now.getMinutes();
+var hours = now.getHours();
+
+var secondHand = document.getElementById("second");  
+var minuteHand = document.getElementById("minute"); 
+var hourHand = document.getElementById("hour");
+
+function setSecondHand(){
+  secondHand.style.transform = "rotate(" + seconds + "deg)";
 }
+setSecondHand();
 
-rotateSecondHand();
-
-function rotateMinuteHand(){
-  var minuteHand = document.getElementById("minute");
-  var counter = 0;
-  var loop = setInterval(function(){
-    counter+=6;
-    minuteHand.style.transform = "rotate(" + counter + "deg)";
-  }, 60000); 
+function setMinuteHand(){
+  minuteHand.style.transform = "rotate(" + (minutes*6) + "deg)";
 }
+setMinuteHand();
 
-rotateMinuteHand();
+function setHourHand(){
+  hourHand.style.transform = "rotate(" + (hours*30) + "deg)";
+}
+setHourHand();
 
-function rotateHourHand(){
-  var hourHand = document.getElementById("hour");
-  var counter = 0;
-  var loop = setInterval(function(){
-    counter+=30;
+
+function moveSecondHand(){
+  var counter = seconds;
+  setInterval(function(){ 
+  counter++;
+  secondHand.style.transform = "rotate(" + counter + "deg)";
+}, 1000); 
+}
+moveSecondHand();
+
+function moveMinuteHand(){
+  var counter = minutes;
+  setInterval(function(){
+  counter+=6;
+  minuteHand.style.transform = "rotate(" + counter + "deg)";
+}, 60000);
+}
+moveMinuteHand();
+
+function moveHourHand(){
+  var counter = hours;
+  setInterval(function(){
+    counter +=30;
     hourHand.style.transform = "rotate(" + counter + "deg)";
-  }, 3.6e+6); 
+  }, 3.6e+6);
 }
-
-rotateHourHand();
+moveHourHand();
